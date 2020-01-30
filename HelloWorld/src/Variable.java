e5479e091e5d36d4b12bd544bd4e785a2fe26b15
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2020-01-31 00:18:24 +0530

    new java codes are here
    Signed-off-by: gunjanmimo <gunjan.mtbpaul@gmail.com>

---
+++ HelloWorld/src/Variable.java
@@ -0,0 +1,9 @@
+public class Variable {
+    public  static  void main(String[]  args){
+        System.out.println("This section is all about variables");
+        int First_no=5;
+        int second_no=34;
+        int total=First_no+second_no;
+        System.out.println(total);
+    }
+}
